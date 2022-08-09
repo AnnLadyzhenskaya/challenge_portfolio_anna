12f09e33d223448ab0492faa3fe5628c6676c0de
Ann Ladyzhenska <ann.ladyzhenskaya@gmail.com>
2022-08-09 14:07:01 +0400

    Start using ChromeDriverManager since executable_path is going to become deprecated

--- test_cases/framework.py
+++ test_cases/framework.py
@@ -2,14 +2,19 @@ import os
 import unittest
 from selenium import webdriver
 from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 class Test(unittest.TestCase):
 
     @classmethod
     def setUp(self):
-        os.chmod(DRIVER_PATH, 755)
-        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+        #os.chmod(DRIVER_PATH, 755)
+        #self.driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+
+        #ChromeDriverManager instead of executable_path
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         self.driver.get('https://scouts-test.futbolkolektyw.pl/en')
         self.driver.fullscreen_window()
         self.driver.implicitly_wait(IMPLICITLY_WAIT)
@@ -20,6 +25,3 @@ class Test(unittest.TestCase):
 
     def test_print_nice_words(self):
         print("WELL DONE!!!!!!!!!")
-
-    # Element of the first task: Try to search the Internet yourself how to get rid of the error:
-    # "DeprecationWarning: executable_path has been deprecated, please pass in a Service object"
